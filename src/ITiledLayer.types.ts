96a9ce44c9cab75ad4e29577af82190e231e0f28
wk0 <9341565+wk0@users.noreply.github.com>
2026-01-16 14:48:54 -0500

    pretty

--- src/ITiledLayer.types.ts
+++ src/ITiledLayer.types.ts
@@ -3,4 +3,7 @@ import { ITiledMapObjectLayer } from './ITiledMapObjectLayer';
 import { ITiledMapInfiniteTileLayer, ITiledMapRestrictedTileLayer } from './ITiledMapTileLayer';
 
 export const ITiledMapInfiniteLayer = z.union([ITiledMapInfiniteTileLayer, ITiledMapObjectLayer]);
-export const ITiledMapRestrictedLayer = z.union([ITiledMapRestrictedTileLayer, ITiledMapObjectLayer]);
+export const ITiledMapRestrictedLayer = z.union([
+  ITiledMapRestrictedTileLayer,
+  ITiledMapObjectLayer,
+]);
